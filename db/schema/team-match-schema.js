db4abc503aab681523278b34e693fc6a45f4c773
Kaushal Kapasi <kaushal.kapasi@td.com>
2017-01-11 15:56:51 -0500

    updated team match schema

--- db/schema/team-match-schema.js
+++ db/schema/team-match-schema.js
@@ -3,7 +3,8 @@
 module.exports = (Schema) => {
 
     let teamsMatchesSchema = new Schema({
-        number: Number,
+    	event: String,
+        match: Number,
         team: Number,
         /* Autonomous Mode */
         auto_fuel_high: Number,     // 1 high = 1 point or 1 kPa
@@ -20,8 +21,9 @@ module.exports = (Schema) => {
         tele_rotor: Number,
         hoppers: Number,
         /* Hang */
-        take_off_attempt: Boolean,
+	take_off_attempt: Boolean,
         take_off_ready: Boolean,
+	take_off_time: Number,
         /* Bonus */
         pressure_bonus: Boolean,
         rotor_bonus: Boolean,
