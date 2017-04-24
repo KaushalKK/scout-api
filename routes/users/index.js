1b26e963c76519481744c63d8464d870ef28de52
Kaushal Kapasi <kaushal.kapasi@td.com>
2017-04-24 16:09:06 -0400

    adding login route

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
-let bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const fs = require("fs");
 
 module.exports = (router, db) => {
     const user = db.models.user;
@@ -8,6 +10,7 @@ module.exports = (router, db) => {
     return {
         "configureRoutes": function () {
             const resource = "/user";
+            const cert = fs.readFileSync("keys/public_key");
 
             router.put(resource, (req, res) => {
                 let userInput = {
@@ -24,6 +27,42 @@ module.exports = (router, db) => {
                     })
                     .catch(() => {
                         res.status(400).send('Failed to create user.');
+                    });
+            });
+
+            router.post(resource + "/login", (req, res) => {
+                let loginResult = false;
+                let options = {
+                    "algorithm": "RS256",
+                    "expiresIn": "1h",
+                    "issuer": "frcscout"
+                };
+
+                let username = req.body.username;
+                let password = req.body.password;
+
+                user.findOne({ alias: username })
+                    .then((userDetails) => {
+                        loginResult = bcrypt.compareSync(password, userDetails.password);
+
+                        let payload = {
+                            user: username,
+                            team: userDetails.team
+                        };
+
+
+                        if (loginResult) {
+                            jwt.sign(payload, cert, options, (token) => {
+                                res.cookie = token;
+                                res.send(200).send(token);
+                            });
+                        } else {
+                            throw new Error('invalidPassword');
+                        }
+                    })
+                    .catch(() => {
+                        loginResult = false;
+                        res.status(401).send('Incorrect username or password');
                     });
             });
         }
